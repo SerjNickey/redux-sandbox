69f4146d0f983e7b16ec7ab4f35e9d850efcdf12
SerjNickey <serjnickey@gmail.com>
2019-02-11 01:24:25 +0300

    Commit 1

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import reducer from './reducer';
+import { Provider } from 'react-redux';
+import App from './components/App';
+
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
